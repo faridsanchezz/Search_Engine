7d52e94a7ee6d1b08c1f634639d72b73aa172207
faridsanchez <sz@>
2024-11-14 10:58:33 +0000

    add WordStoreManagerV2

--- Crawler/src/main/java/Main/Main.java
+++ Crawler/src/main/java/Main/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
-		String datalakePath = "datalake/";
+		String datalakePath = "";
 		int numBooks = 1;
 		Filter filter = new LanguageFilter();
 		Downloader downloader = new BookDownloader(filter);
